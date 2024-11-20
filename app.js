0e1f362f1f5405d3d635d5c0cbc11e0523e42f10
hyejinpark0 <hyejinp0908@gmail.com>
2024-11-19 20:24:21 -0800

    create session and auth

--- app.js
+++ app.js
@@ -7,18 +7,69 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 import apiRouter from './routes/api/api.js'
+import usersRouter from './routes/api/users.js';
+
+import sessions from 'express-session'
+import WebAppAuthProvider from 'msal-node-wrapper'
+
+const authConfig = {
+    auth: {
+        clientId: "fc24c815-505b-4c60-8648-8e4a6a8e62b1",
+        authority: "https://login.microsoftonline.com/f6b6dd5b-f02f-441a-99a0-162ac5060bd2",
+        clientSecret: "mtx8Q~qvuLS5fYlkrtOx2_5gctHykk6D2S-_YajL",
+        redirectUri: "/redirect"
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: 3,
+        }
+    }
+};
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 var app = express();
 
+app.enable('trust proxy');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: "this is some secret key I am making up 093u4oih54lkndso8y43hewrdskjf",
+    saveUninitialized: true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}));
+
+const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
+app.use(authProvider.authenticate());
+
 app.use('/api', apiRouter);
+app.use('/users', usersRouter);
+
+
+app.get('/signin', (req, res, next) => {
+    return req.authContext.login({
+        postLoginRedirectUri: "/",
+    })(req, res, next);
+
+});
+app.get('/signout', (req, res, next) => {
+    return req.authContext.logout({
+        postLogoutRedirectUri: "/",
+    })(req, res, next);
+
+});
+app.use(authProvider.interactionErrorHandler());
 
 export default app;
