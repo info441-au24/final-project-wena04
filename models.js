d0e3a999edd6d4aecbc48a405221f664bfaa3c34
labram13 <labram@uw.edu>
2024-11-20 11:21:09 -0800

    added models.js with businessSchema and employee schema. Commented certain parts just to test if business is working first with just business name.

--- models.js
+++ models.js
@@ -8,4 +8,33 @@ await mongoose.connect('mongodb+srv://mikelabz:1234@cluster0.kxlma.mongodb.net/E
 
 console.log("successfully connected to mongodb")
 
+models.Business = mongoose.model('Business', businessSchema)
+
+const businessSchema = new mongoose.Schema({
+    businessName: String, 
+    // employees: {type: mongoose.Schema.Types.ObjectId, ref: "Employee"},
+    // earnings: Number
+})
+
+models.Business = mongoose.model('Business', businessSchema)
+
+console.log("business models created")
+
+// const employeeSchema = new mongoose.Schema({
+//     firstName: String,
+//     secondName: String, 
+//     hoursWorked: Number, 
+//     hourlyWage: Number, 
+//     earnings: Number
+// })
+
+models.Employee = mongoose.model('Employee', employeeSchema)
+
+console.log("employee models created")
+
+
+
+
+
+
 export default models
