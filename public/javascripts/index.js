76c268964d688c7e373279d5ef23aa92273cd052
labram13 <labram@uw.edu>
2024-11-21 18:04:41 -0800

    implemented post for api /business

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,19 @@
 async function addBusiness() {
     const businessName = document.getElementById('business_name_input').value
-    // console.log(businessName)
+    console.log(businessName)
 
     console.log("making request to post new business name")
-    let responseJson = await fetchJSON(`api/business`, {
-        method: "POST",
-        body: {businessName: businessName}
-    })
+
+    // fetchJSON not defined error
+    // copied utils.js file to implement fetchJSON()
+    try {
+        let responseJson = await fetchJSON(`api/business`, {
+            method: "POST", 
+            body: {businessName: businessName}
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+    
 }
--- routes/api/controllers/business.js
+++ routes/api/controllers/business.js
@@ -3,7 +3,24 @@ var router = express.Router()
 
 
 router.post('/', async (req, res) => {
+    try{
+        console.log("Entering /post")
+        console.log("Received from req.body: ", req.body.businessName)
+        
+        const newBusinessName = req.body.businessName
+        
+        const newBusiness = new req.models.Business({
+            businessName: newBusinessName
+        })
 
+        await newBusiness.save()
+        console.log("successfully saved business into db")
+
+        return res.json({status: "success"})
+    } catch (error) {
+        console.log("Error saving new business: ", error)
+        return res.status(500).json({error: error.message})
+    }
 })
 
 export default router
