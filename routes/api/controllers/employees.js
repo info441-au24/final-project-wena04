5459e6721d728e47c7db58275d80f755ab09c1fe
labram13 <labram@uw.edu>
2024-11-27 13:32:01 -0800

    copied from p1

--- routes/api/controllers/employees.js
+++ routes/api/controllers/employees.js
@@ -15,4 +15,28 @@ router.get("/", async (req, res) => {
   }
 });
 
+//FYI We are not including earnings as property of an employee. This will be handled in another api call
+router.post('/', async (req, res) => {
+  try {
+    const employeePost = new req.models.Employee({
+      firstName: req.body.firstName, 
+      lastName: req.body.lastName,
+      hourlyWage: Number(req.body.hourlyWage),
+      hoursWorked: Number(req.body.hoursWorked),
+      businessID: req.models.businessID
+    })
+    await employeePost.save()
+    res.json({
+      status: "success"
+    })
+
+  } catch (error) {
+    console.log("Error saving employee: ", error)
+    res.status(500).json({
+      status: "error", 
+      error: error.message
+    })
+  }
+})
+
 export default router;
