68cda69054344eb69e6a375380587a1d10b3235e
labram13 <labram@uw.edu>
2024-11-26 17:59:01 -0800

    new employee.js file and udpated router to employee.js

--- routes/api/api.js
+++ routes/api/api.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 import businessRouter from "./controllers/business.js";
 import usersRouter from "./controllers/users.js";
+import employeesRouter from "./controllers/employees.js";
 
 router.use("/business", businessRouter);
 router.use("/users", usersRouter);
+router.use("/employees", employeesRouter);
 
 export default router;
