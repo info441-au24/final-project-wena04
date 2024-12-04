35a912bd9a99b5c0db7f1141af25c41ba7aea6cd
wena04 <wena04@uw.edu>
2024-12-03 20:07:28 -0800

    added some styling

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
 async function init() {
-  loadIdentity()
+  loadIdentity();
   try {
-    let identityInfo = await fetchJSON(`api/users/myIdentity`)
+    let identityInfo = await fetchJSON(`api/users/myIdentity`);
     if (identityInfo.status == "loggedin") {
       loadBusinesses();
     }
-  } catch(error) {
-    console.log("User is not logged in: ", error)
+  } catch (error) {
+    console.log("User is not logged in: ", error);
   }
 }
 
@@ -18,19 +18,22 @@ async function addBusiness() {
 
   // fetchJSON not defined error
   // copied utils.js file to implement fetchJSON()
-    let responseJson = await fetchJSON(`api/business`, {
-      method: "POST",
-      body: { businessName: businessName },
-    });
-    console.log("response received. successfully saved business");
+  let responseJson = await fetchJSON(`api/business`, {
+    method: "POST",
+    body: { businessName: businessName },
+  });
+  console.log("response received. successfully saved business");
 
-    if (responseJson.status == "success") {
-      document.getElementById("postStatus").innerText = `Status: ${responseJson.status}`;
-    } else {
-      document.getElementById("postStatus").innerText = `Status: ${responseJson.status} (${responseJson.error})`;
-
-    }
-    loadBusinesses()
+  if (responseJson.status == "success") {
+    document.getElementById(
+      "postStatus"
+    ).innerText = `Status: ${responseJson.status}`;
+  } else {
+    document.getElementById(
+      "postStatus"
+    ).innerText = `Status: ${responseJson.status} (${responseJson.error})`;
+  }
+  loadBusinesses();
 }
 
 async function loadBusinesses() {
@@ -40,12 +43,18 @@ async function loadBusinesses() {
     let businessesHtml = businessesJson
       .map((business) => {
         return `
-        <div class="business">
-          <a href="/businessInfo.html?businessID=${encodeURIComponent(
-            business._id
-          )}">
-            <p>${business.businessName}</p>
-          <a>
+        <div class="col-md-4 mb-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${business.businessName}</h5>
+              <p class="card-text">
+                <strong>Owner:</strong> ${business.username}
+              </p>
+              <a href="/businessInfo.html?businessID=${encodeURIComponent(
+                business._id
+              )}" class="btn btn-primary">Manage Business</a>
+            </div>
+          </div>
         </div>
       `;
       })
